b73647be74d981c71fd5669feade9d0e689081db
H3INM@WIT <heinmin2maw.it@gmail.com>
2024-09-16 17:24:55 +0700

    fix: template theme not updating during the first load in template section

--- static/js/template-resume.js
+++ static/js/template-resume.js
@@ -1,86 +1,91 @@
+document.addEventListener('DOMContentLoaded', function () {
 
 
-let selectedTemplateId = null
-let isHeadshot = true
-let selectedColor = "#434E5E"
-let headshotRadios = document.querySelectorAll('input[name="headshot"]');
-let profiles = document.getElementsByClassName("profile-div")
+    let selectedTemplateId = null
+    let isHeadshot = true
+    let selectedColor = "#00026e"
+    let headshotRadios = document.querySelectorAll('input[name="headshot"]');
+    let profiles = document.getElementsByClassName("profile-div")
 
-// Load cache
-let templateInfo = JSON.parse(localStorage.getItem(CACHE_NAMES.TEMPLATE))
-if (templateInfo) {
-    selectedTemplateId = templateInfo["templateId"]
-    selectedColor = templateInfo["templateTheme"]
+    // Load cache
+    let templateInfo = JSON.parse(localStorage.getItem(CACHE_NAMES.TEMPLATE))
+    if (templateInfo) {
+        selectedTemplateId = templateInfo["templateId"]
+        selectedColor = templateInfo["templateTheme"]
 
-    selectTemplate(selectedTemplateId)
-    changeTemplateTheme(selectedColor);
-
-    isHeadshot = Boolean(templateInfo["isHeadshot"])
-
-    Array.from(profiles).forEach(profile => profile.style.display = isHeadshot ? "block" : "none")
-    if (isHeadshot) {
-        document.getElementById("with-headshot").checked = true;
-    } else {
-
-        document.getElementById("without-headshot").checked = true;
-    }
-}
-
-// color radio 
-document.querySelectorAll(".color-radio").forEach(radio => {
-    radio.addEventListener("click", function () {
-        selectedColor = this.getAttribute("data-color");
+        selectTemplate(selectedTemplateId)
         changeTemplateTheme(selectedColor);
-    });
-});
 
-headshotRadios.forEach(radio => {
-    radio.addEventListener('change', () => {
-        let headshotValue = document.querySelector('input[name="headshot"]:checked').value;
-        console.log(`Selected Value: ${headshotValue}`);
-        isHeadshot = headshotValue === '1' ? 1 : 0;
+        isHeadshot = Boolean(templateInfo["isHeadshot"])
 
         Array.from(profiles).forEach(profile => profile.style.display = isHeadshot ? "block" : "none")
+        if (isHeadshot) {
+            document.getElementById("with-headshot").checked = true;
+        } else {
 
-    });
-})
-function selectTemplate(templateId) {
-    console.log("here")
-    document.querySelectorAll('.resume-template').forEach(card => {
-        card.classList.remove('active');
-    });
-
-    const selectedTemplate = document.getElementById(templateId);
-    if (selectedTemplate) {
-        selectedTemplate.classList.add('active');
-        selectedTemplateId = templateId;
-        console.log("Selected template: " + templateId);
-    }
-}
-
-
-
-
-function changeTemplateTheme(color) {
-    const root = document.documentElement;
-    root.style.setProperty('--template-bg-color', color);
-}
-
-
-
-
-function cachedTemplateInfo() {
-    if (selectedTemplateId !== null) {
-        templateInfo = {
-            "templateId": selectedTemplateId,
-            "isHeadshot": isHeadshot,
-            "templateTheme": selectedColor
+            document.getElementById("without-headshot").checked = true;
         }
-        localStorage.setItem(CACHE_NAMES.TEMPLATE, JSON.stringify(templateInfo));
-        window.location.href = `/resume/section/heading?template_id=${selectedTemplateId}`
-
-    } else {
-        alert("Please choose one of the template.")
     }
 
-}
+    // color radio 
+    document.querySelectorAll(".color-radio").forEach(radio => {
+        radio.addEventListener("click", function () {
+            selectedColor = this.getAttribute("data-color");
+            changeTemplateTheme(selectedColor);
+        });
+    });
+
+    headshotRadios.forEach(radio => {
+        radio.addEventListener('change', () => {
+            let headshotValue = document.querySelector('input[name="headshot"]:checked').value;
+            console.log(`Selected Value: ${headshotValue}`);
+            isHeadshot = headshotValue === '1' ? 1 : 0;
+
+            Array.from(profiles).forEach(profile => profile.style.display = isHeadshot ? "block" : "none")
+
+        });
+    })
+    function selectTemplate(templateId) {
+        console.log("here")
+        document.querySelectorAll('.resume-template').forEach(card => {
+            card.classList.remove('active');
+        });
+
+        const selectedTemplate = document.getElementById(templateId);
+        if (selectedTemplate) {
+            selectedTemplate.classList.add('active');
+            selectedTemplateId = templateId;
+            console.log("Selected template: " + templateId);
+        }
+    }
+
+
+
+
+    function changeTemplateTheme(color) {
+        const root = document.documentElement;
+        console.log("go here")
+        console.log(color)
+        root.style.setProperty('--template-bg-color', color);
+    }
+
+
+
+
+    function cachedTemplateInfo() {
+        if (selectedTemplateId !== null) {
+            templateInfo = {
+                "templateId": selectedTemplateId,
+                "isHeadshot": isHeadshot,
+                "templateTheme": selectedColor
+            }
+            localStorage.setItem(CACHE_NAMES.TEMPLATE, JSON.stringify(templateInfo));
+            window.location.href = `/resume/section/heading?template_id=${selectedTemplateId}`
+
+        } else {
+            alert("Please choose one of the template.")
+        }
+
+    }
+
+})
